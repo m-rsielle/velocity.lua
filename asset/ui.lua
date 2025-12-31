6053f6eeb4d8dc898f48eca07f09300b80686ec2
ricoo <marvx35@gmail.com>
2025-12-31 11:16:30 -0500

    Update ui.lua

--- asset/ui.lua
+++ asset/ui.lua
@@ -494,11 +494,15 @@ function library:CreateWindow(name, size, hidebutton)
         tab.TabButton.Name = tab.name
         tab.TabButton.TextSize = window.theme.fontsize
         tab.TabButton.BackgroundColor3 = window.theme.navcolor
+        tab.TabPadding = Instance.new("UIPadding", tab.TabButton)
+        tab.TabPadding.PaddingLeft = UDim.new(0, 12)
+        tab.TabPadding.PaddingRight = UDim.new(0, 8)
+
         tab.Highlight = Instance.new("Frame", tab.TabButton)
         tab.Highlight.Name = "highlight"
         tab.Highlight.AnchorPoint = Vector2.new(0, 0)
-        tab.Highlight.Position = UDim2.fromOffset(0, 0)
-        tab.Highlight.Size = UDim2.new(0, 3, 1, 0)
+        tab.Highlight.Position = UDim2.fromOffset(0, 4)
+        tab.Highlight.Size = UDim2.new(0, 3, 1, -8)
         tab.Highlight.BorderSizePixel = 0
         tab.Highlight.BackgroundColor3 = window.theme.accentcolor
         tab.Highlight.Visible = false
